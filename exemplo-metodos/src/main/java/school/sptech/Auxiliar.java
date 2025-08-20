bd5f476f67e1ad438587df279991b80f281fb15f
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-08-20 17:15:42 -0300

    Method with argument

--- exemplo-metodos/src/main/java/school/sptech/Auxiliar.java
+++ exemplo-metodos/src/main/java/school/sptech/Auxiliar.java
@@ -4,4 +4,8 @@ public class Auxiliar {
     void exibirBomDia() {
         System.out.println("Bom dia Jovem!");
     }
+
+    void exibirBomDia(String nome) {
+        System.out.println("Bom dia, " + nome);
+    }
 }
--- exemplo-metodos/src/main/java/school/sptech/main.java
+++ exemplo-metodos/src/main/java/school/sptech/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         Auxiliar auxiliar = new Auxiliar();
         auxiliar.exibirBomDia();
+        auxiliar.exibirBomDia("Anne Yukari Yamasaki");
     }
 }
