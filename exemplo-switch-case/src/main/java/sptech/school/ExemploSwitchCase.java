2d52825dd2eed4867d3b688088e2776b54e2344b
F-Shouter <yukari.yamasaki1809@gmail.com>
2025-09-03 17:17:48 -0300

    novo

---
+++ exemplo-switch-case/src/main/java/sptech/school/ExemploSwitchCase.java
@@ -0,0 +1,11 @@
+package sptech.school;
+
+import java.util.Scanner;
+
+public class ExemploSwitchCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        Integer numero = scanner.nextInt();
+    }
+}
